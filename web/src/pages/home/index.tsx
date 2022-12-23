70087d8f6a6aeeb4bdc2c4175b3599dfa0ed6604
htllog <118370026+htllog@users.noreply.github.com>
2022-12-23 10:45:10 +0800

    Resolve loop outlet
    Resolve loop outlet

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -11,33 +11,35 @@ const Home = () => {
   const routerTabBarContent = (list: RoutePrperty[]) => {
     return list.map((item, index) => {
       return (
-        <div key={index} className={styles.content}>
-          <div className={styles.contentTab}>
-            {item.children?.map((childrenItem, childrenIndex) => {
-              return (
-                <Link
-                  key={childrenIndex}
-                  to={childrenItem.path}
-                  onClick={() => {
-                    setClickIndex(childrenIndex);
-                  }}
-                  className={
-                    clickIndex === childrenIndex
-                      ? styles.contentTabBarShow
-                      : styles.contentTabBar
-                  }
-                >
-                  {childrenItem.title}
-                </Link>
-              );
-            })}
-          </div>
-          <Outlet />
+        <div className={styles.contentTab} key={index}>
+          {item.children?.map((childrenItem, childrenIndex) => {
+            return (
+              <Link
+                key={childrenIndex}
+                to={childrenItem.path}
+                onClick={() => {
+                  setClickIndex(childrenIndex);
+                }}
+                className={
+                  clickIndex === childrenIndex
+                    ? styles.contentTabBarShow
+                    : styles.contentTabBar
+                }
+              >
+                {childrenItem.title}
+              </Link>
+            );
+          })}
         </div>
       );
     });
   };
 
-  return <div>{routerTabBarContent(routerArray)}</div>;
+  return (
+    <div className={styles.content}>
+      {routerTabBarContent(routerArray)}
+      <Outlet />
+    </div>
+  );
 };
 export default Home;
