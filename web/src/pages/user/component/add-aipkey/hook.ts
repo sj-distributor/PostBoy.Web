d0cfd77f1f5ff7884f787d5c78cc894f3c484b80
TRACY.W <109784975+Tracy-Wei@users.noreply.github.com>
2023-02-18 12:24:53 +0800

    update code
    update code

--- web/src/pages/user/component/add-aipkey/hook.ts
+++ web/src/pages/user/component/add-aipkey/hook.ts
@@ -1,3 +1,4 @@
+import { clone, isEmpty } from "ramda"
 import { useState } from "react"
 import {
   GetUserApikeys,
@@ -16,25 +17,31 @@ const useAction = (props: {
   const [apiKey, setAipKey] = useState<string>("")
   const [description, setDescription] = useState<string>("")
 
-  const addApiKeySubmit = () => {
-    PostUserApikeysAdd({
+  const addApiKeySubmit = async () => {
+    await PostUserApikeysAdd({
       apiKey: apiKey,
       description: description,
       userAccountId: userAccountId,
-    }).then(() => {
-      onAddApikeyCancel()
-      GetUserApikeys(userAccountId).then((res) => {
-        console.log("Res", res, userApikeyList)
+    }).then(async () => {
+      await GetUserApikeys(userAccountId).then((res) => {
         if (!!res) {
-          const newList = userApikeyList.map((items) => {
-            if (items[0].userAccountId === userAccountId) {
-              items = res
-            }
-            return items
-          })
-          setUserApikey(newList)
+          if (isEmpty(userApikeyList[0])) {
+            const newList = clone(userApikeyList)
+            newList[0] = res
+            setUserApikey(newList)
+          } else {
+            const userApikeyListClone = clone(userApikeyList)
+            const newList = userApikeyListClone.map((items) => {
+              if (items[0].userAccountId === res[0].userAccountId) {
+                items = res
+              }
+              return items
+            })
+            setUserApikey(newList)
+          }
         }
       })
+      onAddApikeyCancel()
     })
 
     setAipKey("")
