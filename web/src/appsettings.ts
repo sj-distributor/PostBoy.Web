93e31887339938267c98d5281cb55dff9b04aa42
TRACY.W <109784975+Tracy-Wei@users.noreply.github.com>
2023-03-15 09:13:33 +0800

    chang web page title
    修改网页title的名字

--- web/src/appsettings.ts
+++ web/src/appsettings.ts
@@ -1,22 +1,24 @@
 export interface AppSettings {
-  serverUrl: string;
+  serverUrl: string
+  webName: string
 }
 
-const settings = (window as any).appSettings;
+const settings = (window as any).appSettings
 
 export async function InitialAppSetting() {
-  if ((window as any).appSettings) return (window as any).appSettings;
+  if ((window as any).appSettings) return (window as any).appSettings
 
   await fetch("../appsetting.json", {
     headers: {
       "Content-Type": "application/json",
-      Accept: "application/json"
-    }
+      Accept: "application/json",
+    },
   })
     .then((res) => res.json())
     .then((res: AppSettings) => {
-      (window as any).appSettings = res;
-    });
+      document.title = res.webName
+      ;(window as any).appSettings = res
+    })
 }
 
-export default settings as AppSettings;
+export default settings as AppSettings
