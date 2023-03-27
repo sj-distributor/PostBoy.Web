0da010091ea8728c8d378fb674424c5b3a505094
zirui liu <maxon.liu@sjfood.com>
2023-03-27 10:59:33 +0800

    Update hook.ts

--- web/src/pages/email/hook.ts
+++ web/src/pages/email/hook.ts
@@ -182,7 +182,7 @@ const useAction = (
           setJobSetting({
             timezone: timeZone[timeZoneValue].convertTimeZone,
           })
-          setChoosenJobSetting("")
+          setChoosenJobSetting("发送类型: 即时发送")
           editor && editor.setHtml("<p></p>")
           setEmailCopyToArr([])
           setEmailToArr([])
@@ -476,7 +476,7 @@ const useAction = (
         setJobSetting({
           timezone: timeZone[timeZoneValue].convertTimeZone,
         })
-        setChoosenJobSetting("")
+        setChoosenJobSetting("发送类型: 即时发送")
         break
       }
       case MessageJobSendType.Delayed: {
