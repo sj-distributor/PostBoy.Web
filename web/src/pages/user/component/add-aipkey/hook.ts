fa3ce3aff8e40b35c5bf76ea6997456a93bbfbae
TRACY.W <109784975+Tracy-Wei@users.noreply.github.com>
2023-04-07 16:55:44 +0800

    优化用户管理页面的UI和修复添加apikey逻辑问题
    优化用户管理页面的UI和修复添加apikey逻辑问题

--- web/src/pages/user/component/add-aipkey/hook.ts
+++ web/src/pages/user/component/add-aipkey/hook.ts
@@ -1,4 +1,5 @@
-import { clone, isEmpty } from "ramda"
+import useBoolean, { Actions } from "ahooks/lib/useBoolean"
+import { isEmpty } from "ramda"
 import { useState } from "react"
 import {
   GetUserApikeys,
@@ -11,27 +12,35 @@ const useAction = (props: {
   onAddApikeyCancel: () => void
   userApikeyList: IUserApikeysResponse[][]
   setUserApikey: React.Dispatch<React.SetStateAction<IUserApikeysResponse[][]>>
+  successAction: Actions
 }) => {
-  const { userAccountId, onAddApikeyCancel, userApikeyList, setUserApikey } =
-    props
+  const {
+    userAccountId,
+    onAddApikeyCancel,
+    userApikeyList,
+    setUserApikey,
+    successAction,
+  } = props
   const [apiKey, setAipKey] = useState<string>("")
   const [description, setDescription] = useState<string>("")
+  const [isLoading, isLoadingAction] = useBoolean(false)
 
   const addApiKeySubmit = async () => {
+    isLoadingAction.setTrue()
     await PostUserApikeysAdd({
       apiKey: apiKey,
       description: description,
       userAccountId: userAccountId,
-    }).then(async () => {
-      await GetUserApikeys(userAccountId).then((res) => {
+    }).then(() => {
+      onAddApikeyCancel()
+      successAction.setTrue()
+      isLoadingAction.setFalse()
+
+      GetUserApikeys(userAccountId).then((res) => {
         if (!!res) {
-          if (isEmpty(userApikeyList[0])) {
-            const newList = clone(userApikeyList)
-            newList[0] = res
-            setUserApikey(newList)
-          } else {
-            const userApikeyListClone = clone(userApikeyList)
-            const newList = userApikeyListClone.map((items) => {
+          if (!isEmpty(userApikeyList)) {
+            const newList = userApikeyList.map((items) => {
+              items.map((x) => x.userAccountId)
               if (items[0].userAccountId === res[0].userAccountId) {
                 items = res
               }
@@ -41,14 +50,20 @@ const useAction = (props: {
           }
         }
       })
-      onAddApikeyCancel()
     })
 
     setAipKey("")
     setDescription("")
   }
 
-  return { apiKey, setAipKey, description, setDescription, addApiKeySubmit }
+  return {
+    apiKey,
+    setAipKey,
+    description,
+    setDescription,
+    addApiKeySubmit,
+    isLoading,
+  }
 }
 
 export default useAction
--- web/src/pages/user/component/registration/index.tsx
+++ web/src/pages/user/component/registration/index.tsx
@@ -1,20 +1,41 @@
 import styles from "./index.module.scss"
-import { Button, TextField } from "@mui/material"
+import { Button, CircularProgress, TextField } from "@mui/material"
 import useAction from "./hook"
 import { IUserResponse } from "../../../../dtos/user-management"
 import { memo } from "react"
+import { Actions } from "ahooks/lib/useBoolean"
 
 const RegistrationPopup = memo(
   (props: {
     onRegisterCancel: () => void
     setUsersList: React.Dispatch<React.SetStateAction<IUserResponse[]>>
+    successAction: Actions
   }) => {
-    const { onRegisterCancel, setUsersList } = props
-    const { username, setUsername, password, setPassword, registerSubmit } =
-      useAction({ onRegisterCancel, setUsersList })
+    const { onRegisterCancel, setUsersList, successAction } = props
+
+    const {
+      username,
+      setUsername,
+      password,
+      setPassword,
+      registerSubmit,
+      isLoading,
+    } = useAction({ onRegisterCancel, setUsersList, successAction })
 
     return (
       <div className={styles.pageWrap}>
+        {isLoading && (
+          <div className={styles.progress}>
+            <CircularProgress
+              color="primary"
+              sx={{
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+              }}
+            />
+          </div>
+        )}
         <div className={styles.loginBox}>
           <div className={styles.signInTitleBox}>
             <div className={styles.title}>Register</div>
