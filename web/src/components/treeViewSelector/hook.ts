0b98e98142aeaadb485499b3f2e9cac26616d41b
happyshower <1847254608@qq.com>
2023-08-21 13:55:56 +0800

    tree component use webworker
    tree component使用webworker

--- web/src/components/treeViewSelector/hook.ts
+++ web/src/components/treeViewSelector/hook.ts
@@ -1,6 +1,6 @@
-import { useMap, useThrottle, useThrottleEffect } from "ahooks";
-import { clone, difference, differenceWith, remove } from "ramda";
-import { useEffect, useState, useTransition, startTransition } from "react";
+import { useMap, useThrottle } from "ahooks";
+import { clone } from "ramda";
+import { useEffect, useState } from "react";
 import {
   ClickType,
   DepartmentAndUserType,
@@ -8,7 +8,8 @@ import {
   IDepartmentAndUserListValue,
 } from "../../dtos/enterprise";
 import useDeptUserData from "../../hooks/deptUserData";
-import { ITreeViewHookProps, SourceType } from "./props";
+import { ITreeViewHookProps } from "./props";
+import { MyWorker } from "./webWorker";
 
 const useAction = ({
   appId,
@@ -73,103 +74,103 @@ const useAction = ({
       : canSelect === DeptUserCanSelectStatus.User;
   };
 
-  const setAllChildrenById = (
-    id: string | number,
-    childrenList: IDepartmentAndUserListValue[],
-    isSelected: boolean
-  ) => {
-    const mapItem = foldMapGetter(id);
+  // const setAllChildrenById = (
+  //   id: string | number,
+  //   childrenList: IDepartmentAndUserListValue[],
+  //   isSelected: boolean
+  // ) => {
+  //   const mapItem = foldMapGetter(id);
 
-    //向下
-    mapItem &&
-      mapItem.children.forEach((item) => {
-        const innerItem = foldMapGetter(getUniqueId(item));
-        if (innerItem) {
-          const flattenItem = flattenList.find(
-            (x) => getUniqueId(x) === getUniqueId(innerItem)
-          );
-          flattenItem && childrenList.push(flattenItem);
-          innerItem.children.length > 0 &&
-            setAllChildrenById(
-              getUniqueId(innerItem),
-              childrenList,
-              isSelected
-            );
-        }
-      });
+  //   //向下
+  //   mapItem &&
+  //     mapItem.children.forEach((item) => {
+  //       const innerItem = foldMapGetter(getUniqueId(item));
+  //       if (innerItem) {
+  //         const flattenItem = flattenList.find(
+  //           (x) => getUniqueId(x) === getUniqueId(innerItem)
+  //         );
+  //         flattenItem && childrenList.push(flattenItem);
+  //         innerItem.children.length > 0 &&
+  //           setAllChildrenById(
+  //             getUniqueId(innerItem),
+  //             childrenList,
+  //             isSelected
+  //           );
+  //       }
+  //     });
 
-    return childrenList;
-  };
+  //   return childrenList;
+  // };
 
-  const handleIndeterminateList = (
-    clickedItem: IDepartmentAndUserListValue,
-    selectedList: IDepartmentAndUserListValue[],
-    indeterminateList: IDepartmentAndUserListValue[]
-  ) => {
-    /*
-      处理indeterminateList横杠列表的方法
-      找出所有父数据的children是否全选中/全不选中(直接对比selectedList即可) 或 有杠 (此处处理顺序必须从点击项开始往上)
-      只要数据符合条件便set入resultList
-      return一个resultList
-    */
-    const resultList: IDepartmentAndUserListValue[] = [...indeterminateList];
-    const idRouteList = clickedItem.idRoute?.slice().reverse();
+  // const handleIndeterminateList = (
+  //   clickedItem: IDepartmentAndUserListValue,
+  //   selectedList: IDepartmentAndUserListValue[],
+  //   indeterminateList: IDepartmentAndUserListValue[]
+  // ) => {
+  //   /*
+  //     处理indeterminateList横杠列表的方法
+  //     找出所有父数据的children是否全选中/全不选中(直接对比selectedList即可) 或 有杠 (此处处理顺序必须从点击项开始往上)
+  //     只要数据符合条件便set入resultList
+  //     return一个resultList
+  //   */
+  //   const resultList: IDepartmentAndUserListValue[] = [...indeterminateList];
+  //   const idRouteList = clickedItem.idRoute?.slice().reverse();
 
-    idRouteList?.forEach((item) => {
-      const idIndex = (item: number) =>
-        clickedItem.idRoute?.findIndex((x) => x === item) ?? 0;
-      const mapItem = foldMapGetter(
-        `${item}${clickedItem.idRoute?.slice(0, idIndex(item) + 1).join("")}`
-      );
+  //   idRouteList?.forEach((item) => {
+  //     const idIndex = (item: number) =>
+  //       clickedItem.idRoute?.findIndex((x) => x === item) ?? 0;
+  //     const mapItem = foldMapGetter(
+  //       `${item}${clickedItem.idRoute?.slice(0, idIndex(item) + 1).join("")}`
+  //     );
 
-      if (mapItem) {
-        const idSelectedList = selectedList.map((x) => x.id);
-        const idIndeterminateList = resultList.map((x) => x.id);
-        const determine = {
-          allSelected: mapItem?.children.every((child) =>
-            idSelectedList.includes(child.id)
-          ),
-          allNotSelected: mapItem?.children.every(
-            (child) => !idSelectedList.includes(child.id)
-          ),
-          someIndeterminate: mapItem?.children.some((child) =>
-            idIndeterminateList.includes(child.id)
-          ),
-        };
+  //     if (mapItem) {
+  //       const idSelectedList = selectedList.map((x) => x.id);
+  //       const idIndeterminateList = resultList.map((x) => x.id);
+  //       const determine = {
+  //         allSelected: mapItem?.children.every((child) =>
+  //           idSelectedList.includes(child.id)
+  //         ),
+  //         allNotSelected: mapItem?.children.every(
+  //           (child) => !idSelectedList.includes(child.id)
+  //         ),
+  //         someIndeterminate: mapItem?.children.some((child) =>
+  //           idIndeterminateList.includes(child.id)
+  //         ),
+  //       };
 
-        const activeIndex = resultList.findIndex((x) => x.id === mapItem.id);
-        determine.allSelected &&
-          !determine.allNotSelected &&
-          !selectedList.some((item) => item.id === mapItem.id) &&
-          selectedList.push(mapItem);
-        determine.someIndeterminate ||
-        (!determine.allSelected && !determine.allNotSelected)
-          ? activeIndex === -1 && resultList.push(mapItem)
-          : activeIndex > -1 && resultList.splice(activeIndex, 1);
-      }
-    });
+  //       const activeIndex = resultList.findIndex((x) => x.id === mapItem.id);
+  //       determine.allSelected &&
+  //         !determine.allNotSelected &&
+  //         !selectedList.some((item) => item.id === mapItem.id) &&
+  //         selectedList.push(mapItem);
+  //       determine.someIndeterminate ||
+  //       (!determine.allSelected && !determine.allNotSelected)
+  //         ? activeIndex === -1 && resultList.push(mapItem)
+  //         : activeIndex > -1 && resultList.splice(activeIndex, 1);
+  //     }
+  //   });
 
-    return resultList;
-  };
+  //   return resultList;
+  // };
 
-  const handleMapUpdate = (
-    selectedList: IDepartmentAndUserListValue[],
-    indeterminateList: IDepartmentAndUserListValue[]
-  ) => {
-    for (const [key, value] of foldMap.entries()) {
-      const selectedListItem = selectedList.find(
-        (item) => item.id === value.id
-      );
-      const indeterminateListItem = indeterminateList.find(
-        (item) => item.id === value.id
-      );
-      foldMapSetter(key, {
-        ...value,
-        selected: !!selectedListItem,
-        indeterminate: !!indeterminateListItem,
-      });
-    }
-  };
+  // const handleMapUpdate = (
+  //   selectedList: IDepartmentAndUserListValue[],
+  //   indeterminateList: IDepartmentAndUserListValue[]
+  // ) => {
+  //   for (const [key, value] of foldMap.entries()) {
+  //     const selectedListItem = selectedList.find(
+  //       (item) => item.id === value.id
+  //     );
+  //     const indeterminateListItem = indeterminateList.find(
+  //       (item) => item.id === value.id
+  //     );
+  //     foldMapSetter(key, {
+  //       ...value,
+  //       selected: !!selectedListItem,
+  //       indeterminate: !!indeterminateListItem,
+  //     });
+  //   }
+  // };
 
   // 处理部门列表点击选择或者展开
   const handleDeptOrUserClick = (
@@ -186,46 +187,74 @@ const useAction = ({
       ? clickedList
       : [clickedList];
 
-    for (const value of clickedItem) {
-      const mapItem = foldMapGetter(getUniqueId(value));
+    const worker = MyWorker();
 
-      if (mapItem) {
-        if (type === ClickType.Collapse) {
-          // 折叠
-          foldMapSetter(getUniqueId(value), {
-            ...mapItem,
-            isCollapsed: !mapItem.isCollapsed,
-          });
-        } else {
-          const updateSelectedList = [
-            ...setAllChildrenById(getUniqueId(value), [], true),
-            ...clickedItem,
-          ];
+    worker.postMessage({
+      type,
+      clickedItem,
+      selectedList: clone(selectedList),
+      foldMap,
+      indeterminateList: clone(indeterminateList),
+      flattenList: clone(flattenList),
+    });
 
-          let newSelectedList = mapItem.selected
-            ? selectedList.filter(
-                (value) =>
-                  !updateSelectedList.some((item) => item.id === value.id)
-              )
-            : [...selectedList, ...updateSelectedList];
-
-          const newIndeterminateList = handleIndeterminateList(
-            mapItem,
-            newSelectedList,
-            indeterminateList
-          );
-
-          newSelectedList = newSelectedList.filter(
-            (value) =>
-              !newIndeterminateList.some((item) => item.id === value.id)
-          );
-
-          setSelectedList(newSelectedList);
-          setIndeterminateList(newIndeterminateList);
-          handleMapUpdate(newSelectedList, newIndeterminateList);
-        }
+    worker.onmessage = (event: {
+      data: {
+        newSelectedList?: IDepartmentAndUserListValue[];
+        newIndeterminateList?: IDepartmentAndUserListValue[];
+        foldMap: Map<string | number, IDepartmentAndUserListValue>;
+      };
+    }) => {
+      const { newSelectedList, newIndeterminateList, foldMap } = event.data;
+      newSelectedList && setSelectedList(newSelectedList);
+      newIndeterminateList && setIndeterminateList(newIndeterminateList);
+      for (const [key, value] of foldMap.entries()) {
+        foldMapSetter(key, value);
       }
-    }
+    };
+
+    // worker.terminate();
+
+    // for (const value of clickedItem) {
+    //   const mapItem = foldMapGetter(getUniqueId(value));
+
+    //   if (mapItem) {
+    //     if (type === ClickType.Collapse) {
+    //       // 折叠
+    //       foldMapSetter(getUniqueId(value), {
+    //         ...mapItem,
+    //         isCollapsed: !mapItem.isCollapsed,
+    //       });
+    //     } else {
+    //       const updateSelectedList = [
+    //         ...setAllChildrenById(getUniqueId(value), [], true),
+    //         ...clickedItem,
+    //       ];
+
+    //       let newSelectedList = mapItem.selected
+    //         ? selectedList.filter(
+    //             (value) =>
+    //               !updateSelectedList.some((item) => item.id === value.id)
+    //           )
+    //         : [...selectedList, ...updateSelectedList];
+
+    //       const newIndeterminateList = handleIndeterminateList(
+    //         mapItem,
+    //         newSelectedList,
+    //         indeterminateList
+    //       );
+
+    //       newSelectedList = newSelectedList.filter(
+    //         (value) =>
+    //           !newIndeterminateList.some((item) => item.id === value.id)
+    //       );
+
+    //       setSelectedList(newSelectedList);
+    //       setIndeterminateList(newIndeterminateList);
+    //       handleMapUpdate(newSelectedList, newIndeterminateList);
+    //     }
+    //   }
+    // }
   };
 
   const handleClear = (
--- web/src/components/treeViewSelector/webWorker.ts
+++ web/src/components/treeViewSelector/webWorker.ts
@@ -1,7 +1,195 @@
 import WebWorker from "../../hooks/webWorker";
+import { IDepartmentAndUserListValue } from "../../dtos/enterprise";
 
 export const MyWorker = () => {
-  function workerCode(this: Worker) {}
+  function workerCode(this: Worker) {
+    enum ClickType {
+      Collapse,
+      Select,
+    }
+
+    interface IWebWorkerEventRecive {
+      type: ClickType;
+      clickedItem: IDepartmentAndUserListValue[];
+      selectedList: IDepartmentAndUserListValue[];
+      flattenList: IDepartmentAndUserListValue[];
+      indeterminateList: IDepartmentAndUserListValue[];
+      foldMap: Map<string | number, IDepartmentAndUserListValue>;
+    }
+
+    this.onmessage = (event: { data: IWebWorkerEventRecive }) => {
+      // console.log(event);
+      const {
+        type,
+        clickedItem,
+        selectedList,
+        flattenList,
+        indeterminateList,
+        foldMap,
+      } = event.data;
+      console.log(event.data);
+
+      const getUniqueId = (value: IDepartmentAndUserListValue): string => {
+        return `${value.id}${value.idRoute?.join("")}`;
+      };
+
+      const setAllChildrenById = (
+        id: string | number,
+        childrenList: IDepartmentAndUserListValue[],
+        isSelected: boolean
+      ) => {
+        const mapItem = foldMap.get(id);
+
+        //向下
+        mapItem &&
+          mapItem.children.forEach((item) => {
+            const innerItem = foldMap.get(getUniqueId(item));
+            if (innerItem) {
+              const flattenItem = flattenList.find(
+                (x) => getUniqueId(x) === getUniqueId(innerItem)
+              );
+              flattenItem && childrenList.push(flattenItem);
+              innerItem.children.length > 0 &&
+                setAllChildrenById(
+                  getUniqueId(innerItem),
+                  childrenList,
+                  isSelected
+                );
+            }
+          });
+
+        return childrenList;
+      };
+
+      const handleIndeterminateList = (
+        clickedItem: IDepartmentAndUserListValue,
+        selectedList: IDepartmentAndUserListValue[],
+        indeterminateList: IDepartmentAndUserListValue[]
+      ) => {
+        /*
+          处理indeterminateList横杠列表的方法
+          找出所有父数据的children是否全选中/全不选中(直接对比selectedList即可) 或 有杠 (此处处理顺序必须从点击项开始往上)
+          只要数据符合条件便set入resultList
+          return一个resultList
+        */
+        const resultList: IDepartmentAndUserListValue[] = [
+          ...indeterminateList,
+        ];
+        const idRouteList = clickedItem.idRoute?.slice().reverse();
+
+        idRouteList?.forEach((item) => {
+          const idIndex = (item: number) =>
+            clickedItem.idRoute?.findIndex((x) => x === item) ?? 0;
+          const mapItem = foldMap.get(
+            `${item}${clickedItem.idRoute
+              ?.slice(0, idIndex(item) + 1)
+              .join("")}`
+          );
+
+          if (mapItem) {
+            const idSelectedList = selectedList.map((x) => x.id);
+            const idIndeterminateList = resultList.map((x) => x.id);
+            const determine = {
+              allSelected: mapItem?.children.every((child) =>
+                idSelectedList.includes(child.id)
+              ),
+              allNotSelected: mapItem?.children.every(
+                (child) => !idSelectedList.includes(child.id)
+              ),
+              someIndeterminate: mapItem?.children.some((child) =>
+                idIndeterminateList.includes(child.id)
+              ),
+            };
+
+            const activeIndex = resultList.findIndex(
+              (x) => x.id === mapItem.id
+            );
+            determine.allSelected &&
+              !determine.allNotSelected &&
+              !selectedList.some((item) => item.id === mapItem.id) &&
+              selectedList.push(mapItem);
+            determine.someIndeterminate ||
+            (!determine.allSelected && !determine.allNotSelected)
+              ? activeIndex === -1 && resultList.push(mapItem)
+              : activeIndex > -1 && resultList.splice(activeIndex, 1);
+          }
+        });
+
+        return resultList;
+      };
+
+      const handleMapUpdate = (
+        selectedList: IDepartmentAndUserListValue[],
+        indeterminateList: IDepartmentAndUserListValue[]
+      ) => {
+        for (const [key, value] of foldMap.entries()) {
+          const selectedListItem = selectedList.find(
+            (item) => item.id === value.id
+          );
+          const indeterminateListItem = indeterminateList.find(
+            (item) => item.id === value.id
+          );
+          foldMap.set(key, {
+            ...value,
+            selected: !!selectedListItem,
+            indeterminate: !!indeterminateListItem,
+          });
+        }
+      };
+
+      for (const value of clickedItem) {
+        const mapItem = foldMap.get(getUniqueId(value));
+
+        if (mapItem) {
+          if (type === ClickType.Collapse) {
+            // 折叠
+            foldMap.set(getUniqueId(value), {
+              ...mapItem,
+              isCollapsed: !mapItem.isCollapsed,
+            });
+            this.postMessage({
+              foldMap,
+            });
+          } else {
+            const updateSelectedList = [
+              ...setAllChildrenById(getUniqueId(value), [], true),
+              ...clickedItem,
+            ];
+
+            let newSelectedList = mapItem.selected
+              ? selectedList.filter(
+                  (value) =>
+                    !updateSelectedList.some((item) => item.id === value.id)
+                )
+              : [...selectedList, ...updateSelectedList];
+
+            const newIndeterminateList = handleIndeterminateList(
+              mapItem,
+              newSelectedList,
+              indeterminateList
+            );
+
+            newSelectedList = newSelectedList.filter(
+              (value) =>
+                !newIndeterminateList.some((item) => item.id === value.id)
+            );
+
+            // setSelectedList(newSelectedList);
+            // setIndeterminateList(newIndeterminateList);
+            handleMapUpdate(newSelectedList, newIndeterminateList);
+            this.postMessage({
+              newSelectedList,
+              newIndeterminateList,
+              foldMap,
+            });
+          }
+        }
+      }
+    };
+    this.terminate()
+    // this.postMessage()
+    // this.terminate()
+  }
   const myWorker = new WebWorker(workerCode);
   return myWorker;
 };
