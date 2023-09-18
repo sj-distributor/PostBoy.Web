379f245f34e5d6b2c50060ef55265debcc1eac82
Bandi.Z <htllog@163.com>
2023-09-18 15:17:33 +0800

    Update table

--- web/src/pages/role-perimission/components/user-list/hook.ts
+++ web/src/pages/role-perimission/components/user-list/hook.ts
@@ -1,7 +1,43 @@
+import { ChangeEvent, useState } from "react";
+
+interface RowDto {
+  id: number;
+  name: string;
+  date: string;
+}
+
 export const useAction = () => {
-  const handleSearch = () => {
-    console.log("Search initiated.");
+  const initData: RowDto[] = [
+    {
+      id: 1,
+      name: "xxx",
+      date: "2014-12-24 23:12:00",
+    },
+  ];
+
+  const [inputVal, setInputVal] = useState<string>("");
+
+  const [rows, setRows] = useState<any>(initData);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputVal(event.target.value);
   };
 
-  return { handleSearch };
+  const handleSearch = () => {
+    console.log("Search content:", inputVal);
+  };
+
+  const handleDelete = (name: string) => {
+    const updatedRows = rows.filter((row: RowDto) => row.name !== name);
+
+    setRows(updatedRows);
+  };
+
+  return {
+    rows,
+    inputVal,
+    handleInputChange,
+    handleSearch,
+    handleDelete,
+  };
 };
