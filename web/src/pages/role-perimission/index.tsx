517e6d1eeaae76a670f46d4d8882ec2b986ccf37
Bandi.Z <htllog@163.com>
2023-09-19 14:13:06 +0800

    Update code

--- web/src/pages/role-perimission/index.tsx
+++ web/src/pages/role-perimission/index.tsx
@@ -1,22 +1,11 @@
-import { Link, Outlet, useLocation } from "react-router-dom";
-import { RoleFrom } from "./components/role-from";
-import { RolePermissions } from "./components/role-permissions";
-import { UserList } from "./components/user-list";
-import styles from "./index.module.scss";
-import { useContext } from "react";
-import { AdministratorContext } from "../main";
-import { routerArray } from "../../router/elementRoute";
+import { Outlet } from "react-router-dom";
 
-import { ChildProps } from "../../dtos/route";
-import { useAction } from "./hook";
+import styles from "./index.module.scss";
 
 export const RolePermission = () => {
   return (
     <div className={styles.home}>
       <Outlet />
-      {/* {path === "/roles/userList" && <UserList />}
-      {path === "/roles/editRole" && <RoleFrom />}
-      {(path === "/roles/roleList" || path === "/roles") && <RolePermissions />} */}
     </div>
   );
 };
