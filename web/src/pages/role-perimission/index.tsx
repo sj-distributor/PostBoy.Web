1a8f3d10c9dea71da94943981410bcbc9cb7ffca
TED.F <ted.fang@sjfood.com>
2023-11-04 09:13:29 +0800

    Notistack component access

--- web/src/pages/role-perimission/index.tsx
+++ web/src/pages/role-perimission/index.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom";
 
 import styles from "./index.module.scss";
+import { SnackbarProvider } from "notistack";
 
 export const RolePermission = () => {
   return (
     <div className={styles.container}>
-      <Outlet />
+      <SnackbarProvider
+        maxSnack={3}
+        autoHideDuration={3000}
+        anchorOrigin={{ horizontal: "center", vertical: "top" }}
+      >
+        <Outlet />
+      </SnackbarProvider>
     </div>
   );
 };
