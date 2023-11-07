6e5e5a987685a52ae397f90d6dacf62a83ecdd32
wuxin666789 <1356404098@qq.com>
2023-11-07 20:29:32 +0800

    Update index.ts

--- web/src/api/role-user-permissions/index.ts
+++ web/src/api/role-user-permissions/index.ts
@@ -1,12 +1,48 @@
 import {
+  IAddRoleUsersDto,
   IDeleteRole,
+  IDeleteRoleUserRequest,
   IFoundationTreeDto,
   IPageDto,
   IPermissionsDto,
   IRolePermission,
   IRolePermissionDto,
+  IRoleUserItemDto,
+  IRoleUserPageDto,
+  IRoleUserResponse,
 } from "../../dtos/role-user-permissions";
 import { Get, Post } from "../http-client";
+
+// 获取用户list
+export const GetRoleUser = async (data: IRoleUserPageDto) => {
+  return await Get<IRoleUserResponse>(
+    `/api/Security/role/users?PageIndex=${data.PageIndex + 1}&PageSize=${
+      data.PageSize
+    }&RoleId=${data.RoleId}&Keyword=${data.Keyword}`
+  );
+};
+
+// 移除用户
+export const DeleteRoleUser = async (data: IDeleteRoleUserRequest) => {
+  return await Post("/api/Security/role/users/delete", data);
+};
+
+// 添加用户
+export const AddRoleUser = async (data: IAddRoleUsersDto) => {
+  return await Post<IRoleUserItemDto>("/api/Security/role/users/create", data);
+};
+
+//人员层级tree
+export const GetTreeList = async () => {
+  return await Get<IFoundationTreeDto>(
+    "/api/Foundation/department/staff/hierarchy/tree"
+  );
+};
+
+//获取全部角色用户列表
+export const GetRoleUserList = async () => {
+  return await Get<IRoleUserResponse>(`/api/Security/role/users`);
+};
 
 // 获取功能权限
 export const GetPermissions = async () => {
@@ -48,10 +84,4 @@ export const GetRolesList = async (data: IPageDto) => {
 //删除角色
 export const DeleteRoles = async (data: IDeleteRole) => {
   return await Post("/api/Security/roles/delete", data);
-};
-
-export const GetTreeList = async () => {
-  return await Get<IFoundationTreeDto>(
-    "/api/Foundation/department/staff/hierarchy/tree"
-  );
 };
