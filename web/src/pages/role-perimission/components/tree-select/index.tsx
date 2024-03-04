3379dee1e24026e7819321f3a0701ed9ccdac72c
wuxin666789 <1356404098@qq.com>
2024-03-04 16:39:27 +0800

    Update index.tsx

--- web/src/pages/role-perimission/components/tree-select/index.tsx
+++ web/src/pages/role-perimission/components/tree-select/index.tsx
@@ -20,7 +20,7 @@ export const TreeSelectList: React.FC<{
   treeData: TreeNode[];
   roleUserList: IRoleUserItemDto[];
   searchValue: string;
-  ref: React.Ref<TreeSelectRef> | null;
+  ref: any;
   setSelectedData: (data: TreeNode[]) => void;
 }> = React.forwardRef(
   ({ treeData, searchValue, setSelectedData, roleUserList }, ref) => {
